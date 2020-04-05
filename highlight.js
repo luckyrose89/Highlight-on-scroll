cf29f97b5088632340847ea963f1b00a67ffcfc4
avidrutham <divyamathur89@gmail.com>
2020-04-05 20:59:12 +0530

    first commit

---
+++ highlight.js
@@ -0,0 +1,12 @@
+/*
+1- The section in view is highlighted on the nav at the beginning
+2- Define the beginning point to start detecting scroll behaviour. This point is 
+the first item in menu
+3- Detect scroll behaviour:
+a) On scroll down, detect item in viewport
+b) Listen for scroll down behavoiur and detect item in vieport
+c) highlight the item's corresponding nav element.
+
+create a function to detect if current section is in viewport
+create a function to highlight item on nav
+*/
