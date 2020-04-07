0f28dd3daa2799d52445cb54ae5f331a283d49f8
avidrutham <divyamathur89@gmail.com>
2020-04-07 20:04:25 +0530

    add basic banner image changing function

--- highlight.js
+++ highlight.js
@@ -1,12 +1,13 @@
 var header = document.querySelector(".main-header");
-var images = [
-  "./images/blueberry-pic.jpg",
-  "./images/tomato-pic.jpg",
-  "./images/lavendar-pic.jpg",
-  "./images/orange-pic.jpg"
-];
+var images = {
+  about: "./images/blueberry-pic.jpg",
+  history: "./images/tomato-pic.jpg",
+  clients: "./images/lavendar-pic.jpg",
+  contact: "./images/orange-pic.jpg"
+};
+
 var bannerBox = document.querySelector(".fixed-banner__image");
-bannerBox.style.backgroundImage = `url('${images[0]}')`;
+
 var spy = new Gumshoe(".nav-list a", {
   reflow: true,
   offset: function() {
@@ -14,20 +15,18 @@ var spy = new Gumshoe(".nav-list a", {
   }
 });
 
+function setBannerImage(attribute) {
+  let imageLink = images[attribute];
+  return imageLink;
+}
+
 document.addEventListener(
   "gumshoeActivate",
   function(event) {
     // The list item
-    var li = event.target;
-
-    // The link
-    var link = event.detail.link;
-    console.log(link);
-    // The content
-    var content = event.detail.content;
-    console.log(content);
-
-    console.log(li);
+    var listItemData = event.target.getAttribute("data-link");
+    var image = setBannerImage(listItemData);
+    bannerBox.style.backgroundImage = `url('${image}')`;
   },
   false
 );
