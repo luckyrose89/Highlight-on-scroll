dd437f3e8f8d0f44964ea8d12db11391d0e2b678
avidrutham <divyamathur89@gmail.com>
2020-04-07 19:11:06 +0530

    add fixed banner container and event listener for gumshoeactivate links

--- highlight.js
+++ highlight.js
@@ -1,1 +1,33 @@
-var spy = new Gumshoe(".nav-list a");
+var header = document.querySelector(".main-header");
+var images = [
+  "./images/blueberry-pic.jpg",
+  "./images/tomato-pic.jpg",
+  "./images/lavendar-pic.jpg",
+  "./images/orange-pic.jpg"
+];
+var bannerBox = document.querySelector(".fixed-banner__image");
+bannerBox.style.backgroundImage = `url('${images[0]}')`;
+var spy = new Gumshoe(".nav-list a", {
+  reflow: true,
+  offset: function() {
+    return header.getBoundingClientRect().height;
+  }
+});
+
+document.addEventListener(
+  "gumshoeActivate",
+  function(event) {
+    // The list item
+    var li = event.target;
+
+    // The link
+    var link = event.detail.link;
+    console.log(link);
+    // The content
+    var content = event.detail.content;
+    console.log(content);
+
+    console.log(li);
+  },
+  false
+);
